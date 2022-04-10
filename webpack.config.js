22c3e7469720cab9ffd36f4442ec70fd48284733
CyberCastle <cybercastle@gmail.com>
2022-04-10 00:26:50 -0400

    Add brotli assets compression

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const zlib = require('zlib');
+const CompressionPlugin = require('compression-webpack-plugin');
+
+// Webpack configuration to optimize angular build size
+module.exports = {
+    plugins: [
+        new CompressionPlugin({
+            filename: '[path][base].br',
+            // Use brotli compression algorithm: https://github.com/google/brotli
+            algorithm: 'brotliCompress',
+            test: /\.(js|css|html|svg|eot|woff2|woff|ttf)$/,
+            compressionOptions: {
+                params: {
+                    // For more options, check https://nodejs.org/api/zlib.html#zlib_class_brotlioptions
+                    [zlib.constants.BROTLI_PARAM_QUALITY]: 11
+                }
+            },
+            threshold: 10240,
+            minRatio: 0.8,
+            // We only keep the .br assets
+            // deleteOriginalAssets: true
+            deleteOriginalAssets: false
+        })
+    ]
+};
